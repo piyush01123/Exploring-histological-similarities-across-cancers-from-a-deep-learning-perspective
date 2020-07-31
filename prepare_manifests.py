dbf079093d33e8c1310283c466a6a9bb2525389e
piyush-kgp <piyushsinghkgpian@gmail.com>
2020-08-01 01:20:00 +0530

    divide manifest into multiple manifests

---
+++ prepare_manifests.py
@@ -0,0 +1,21 @@
+
+import os, glob, pandas as pd
+done = open("done.txt", 'r').read().split('\n')
+done = [i.split('/')[-1] for i in done]
+
+organs = ["Breast", "Colorectal", "Kidney", "Liver", "Lung", "Prostate", "Stomach"]
+manifests = [glob.glob(os.path.join('Manifests/', organ+"*.txt")) for organ in organs]
+manifests = [j for i in manifests for j in i]
+df = [pd.read_csv(fp, sep='\t') for fp in manifests]
+df = pd.concat(df)
+print(df.shape)
+
+finished = df[df.filename.isin(done)]
+unfinished = df[~df.filename.isin(done)]
+unfinished = unfinished.reset_index(drop=True)
+print(finished.shape, unfinished.shape)
+
+for i in range(len(unfinished)//100+1):
+    print("Partition", i)
+    subset = unfinished.iloc[100*i:100*(i+1)]
+    subset.to_csv("Partition_{}".format(i),sep='\t',index=False)
