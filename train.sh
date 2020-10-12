dc3e757b5ac646fd5bacbfc30d5cbdf4b0d8778d
piyush-kgp <piyushsinghkgpian@gmail.com>
2020-10-12 10:01:33 +0530

    run on other node

--- train.sh
+++ train.sh
@@ -9,13 +9,24 @@
 
 username=piyush
 subtype=STAD
+
 module load cuda/10.0
 module load cudnn/7.3-cuda-10.0
 source ~/v3env/bin/activate
 mkdir -p /ssd_scratch/cvit/${username}/ExptDataJson/
 mkdir -p /ssd_scratch/cvit/${username}/${subtype}/
-rsync -zaPq ecdp2020@10.4.16.73:TCGA_PATCHES/${subtype}/  /ssd_scratch/cvit/${username}/${subtype}/
+rsync -aPzq gnode19: /ssd_scratch/cvit/${username}/${subtype}/  /ssd_scratch/cvit/${username}/${subtype}/
 rsync -aPz ecdp2020@10.4.16.73:ExptDataJson/${subtype}_expt.json /ssd_scratch/cvit/${username}/ExptDataJson/
+
+# python3 expt_data/merge_expt.py \
+#         --train_dir /ssd_scratch/cvit/${username}/${subtype}/train \
+#         --val_dir /ssd_scratch/cvit/${username}/${subtype}/val \
+#         --test_dir /ssd_scratch/cvit/${username}/${subtype}/test \
+#         --expt_train_dir /ssd_scratch/cvit/${username}/${subtype}/train_data_for_expt \
+#         --expt_val_dir /ssd_scratch/cvit/${username}/${subtype}/val_data_for_expt \
+#         --expt_test_dir /ssd_scratch/cvit/${username}/${subtype}/test_data_for_expt \
+#         --expt_json /ssd_scratch/cvit/${username}/ExptDataJson/${subtype}_expt.json
+#
 
 python3 expt_data/move_expt.py \
         --train_dir /ssd_scratch/cvit/${username}/${subtype}/train \
@@ -31,9 +42,9 @@ python patch_cnn.py \
       --train_dir /ssd_scratch/cvit/${username}/${subtype}/train_data_for_expt/ \
       --val_dir /ssd_scratch/cvit/${username}/${subtype}/val_data_for_expt/ \
       --save_prefix ${subtype} \
-      --log_dir /ssd_scratch/cvit/${username}/${subtype}/logs_train/ \
-      --model_save_path /ssd_scratch/cvit/${username}/${subtype}/model_ckpt/ \
-      --num_epochs 40  | tee ./${subtype}/${subtype}_train_log_full_filtered_patches.txt
+      --log_dir /ssd_scratch/cvit/${username}/logs_train/ \
+      --model_save_path /ssd_scratch/cvit/${username}/model_ckpt/ \
+      --num_epochs 40  | tee ${subtype}_train_log_best_hp.txt
 
-
-cp -r /ssd_scratch/cvit/${username}/${subtype}/model_ckpt ~/
+mkdir -p ~/${subtype}
+cp -r /ssd_scratch/cvit/${username}/${subtype}/model_ckpt/ ~/{subtype}/
