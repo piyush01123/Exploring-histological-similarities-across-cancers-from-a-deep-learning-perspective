1ef82384359260785a2a96229a9061972e2b64b8
piyush-kgp <piyushsinghkgpian@gmail.com>
2020-10-12 10:10:53 +0530

    new sbatch script

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -A delta_one
-#SBATCH -n 40
+#SBATCH -n 32
 ##SBATCH --wait-all-nodes=0
 #SBATCH --gres=gpu:4
 #SBATCH --mem-per-cpu=2048
@@ -15,7 +15,7 @@ module load cudnn/7.3-cuda-10.0
 source ~/v3env/bin/activate
 mkdir -p /ssd_scratch/cvit/${username}/ExptDataJson/
 mkdir -p /ssd_scratch/cvit/${username}/${subtype}/
-rsync -aPzq gnode19: /ssd_scratch/cvit/${username}/${subtype}/  /ssd_scratch/cvit/${username}/${subtype}/
+rsync -aPzq gnode19:/ssd_scratch/cvit/${username}/${subtype}/  /ssd_scratch/cvit/${username}/${subtype}/
 rsync -aPz ecdp2020@10.4.16.73:ExptDataJson/${subtype}_expt.json /ssd_scratch/cvit/${username}/ExptDataJson/
 
 # python3 expt_data/merge_expt.py \
