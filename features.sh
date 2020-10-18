f4904704ff34ed95560f32dc69c69043d9cdad46
piyush-kgp <piyushsinghkgpian@gmail.com>
2020-10-18 23:12:42 +0530

    minor edit

--- features.sh
+++ features.sh
@@ -19,7 +19,7 @@ source /home/delta_one/v3env/bin/activate
 for model in BRCA	COAD	KICH	KIRC	KIRP	LIHC	LUAD	LUSC	PRAD	READ	STAD
 do
   ckpt=${model}_best_model.pth
-  mkdir -p ~/Features/${model}_Model
+  mkdir -p /ssd_scratch/cvit/${username}/Features/${model}_Model
   for testset in BRCA	COAD	KICH	KIRC	KIRP	LIHC	LUAD	LUSC	PRAD	READ	STAD
   do
     echo Model=${model}, TestData=${testset}
@@ -28,6 +28,6 @@ do
           --model_checkpoint /ssd_scratch/cvit/${username}/CheckPoints/${model}_best_model.pth \
           --hparam_json best_hparams.json \
           --model_organ ${model} \
-          --h5py_file_path /ssd_scratch/cvit/piyush/Features/${model}_Model/${testset}.h5 \
+          --h5py_file_path /ssd_scratch/cvit/piyush/Features/${model}_Model/${testset}.h5
     done
 done
