b95e751e9cfe0045ba0cbe05ffd54f983b58cc7e
piyush-kgp <piyushsinghkgpian@gmail.com>
2020-10-19 03:28:35 +0530

    purge

--- plot_exp.sh
+++
@@ -1,9 +0,0 @@
-username=piyush
-source /home/delta_one/v3env/bin/activate
-model=BRCA
-python tsne.py \
-        --h5py_files_root /ssd_scratch/cvit/${username}/Features/${model}_Model/ \
-        --test_sets BRCA	COAD	LIHC	READ \
-        --model_organ ${model} \
-        --points_to_use 50 \
-        --outfile ./TSNE_Plot_${model}.jpg
